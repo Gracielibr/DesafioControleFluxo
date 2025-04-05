62279c65a539c3d090a4ca84772529b5ee980107
Gra <gracielibr@hotmail.com>
2025-04-05 14:04:36 -0300

    Contador de Fluxo

---
+++ Contador/README.md
@@ -0,0 +1,36 @@
+# Desafio Controle de Fluxo 
+
+Foi feito com a ajuda das explicações nas aulas do professor
+-[Gleyson Sampaio](https://github.com/glysns)
+- [Desafio Controle de Fluxo](https://github.com/digitalinnovationone/trilha-java-basico/blob/main/desafios/controle-fluxo)
+
+## Código Base
+Abaixo temos o código que servirá de base para a resolução do desafio:
+
+```java
+public class Contador {
+    	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = // Atribuir input do usuário
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = // Atribuir input do usuário
+		
+		try {
+			// Chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		} catch (ParametrosInvalidosException exception) {
+			// Imprimir a mensagem de erro
+		}
+	}
+
+        static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+                // Validar se os parâmetros são válidos
+
+                // Realizar o for para imprimir os números com base nos parâmetros
+        }
+}
+```
---
+++ Contador/src/Contador.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import meu.pacote.ParametrosInvalidosException; 
+
+
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        
+        while (true) {
+            try {
+                
+                System.out.println("Digite o primeiro parâmetro");
+                int parametroUm = terminal.nextInt();
+
+                
+                System.out.println("Digite o segundo parâmetro");
+                int parametroDois = terminal.nextInt();
+
+                
+                contar(parametroUm, parametroDois);
+
+                
+                break; 
+
+            } catch (ParametrosInvalidosException exception) {
+                System.out.println(exception.getMessage());
+
+            } catch (Exception e) {
+                System.out.println("Por favor, insira números inteiros válidos.");
+                terminal.nextLine(); 
+            }
+        }
+
+        terminal.close(); 
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+            if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println("Imprimir o número: " + i);
+        }
+    }
+}
