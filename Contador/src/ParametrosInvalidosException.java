6c7af6f9d2bce6fc31f9ad6ce9ac8dc85e1b34f7
Gracielibr <gracielibr@hotmail.com>
2025-04-07 04:25:16 -0300

    ParametrosInvalidosException.java

--- Contador/src/ParametrosInvalidosException.java
+++ Contador/src/ParametrosInvalidosException.java
@@ -1,5 +1,5 @@
 public class ParametrosInvalidosException extends Exception {
     public ParametrosInvalidosException(String message) {
-        super(message);  // Passa a mensagem para o construtor da superclasse Exception
+        super(message);  
     }
 }
